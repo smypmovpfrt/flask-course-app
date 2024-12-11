cf75a7232333e262d69b601a8a150755f4a3ad7c
Anton <bcp.ant@gmail.com>
2024-12-11 22:49:05 +0300

    startdayone

---
+++ flask-course-app/arguments.py
@@ -0,0 +1,3 @@
+site_name = 'Преобразователь форматов'
+input_menu = ['PNG','JPG','BMP', 'WEBM']
+output_menu = ['PNG','JPG','BMP', 'WEBM']
--- flask-course-app/flask-app.py
+++ flask-course-app/flask-app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from arguments import site_name, output_menu
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html", title = site_name)
+
+@app.route("/about")
+def about():
+    return "<h1>SASI!!!</h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
-    

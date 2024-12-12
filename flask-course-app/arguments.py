4327513b2dd505f82505482ddf4b49668a6643f0
Anton <bcp.ant@gmail.com>
2024-12-12 13:15:09 +0300

    update

--- flask-course-app/arguments.py
+++ flask-course-app/arguments.py
@@ -1,3 +1,5 @@
 site_name = 'Преобразователь форматов'
 input_menu = ['PNG','JPG','BMP', 'WEBM']
 output_menu = ['PNG','JPG','BMP', 'WEBM']
+site_menu = [{"name": "Эта страница", "url": "index"},
+             {"name": "Другая страница", "url": "links"}]
--- flask-course-app/flask-app.py
+++ flask-course-app/flask-app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
-from arguments import site_name, output_menu
+from flask import Flask, render_template, url_for
+from arguments import site_name, output_menu, site_menu
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html", title = site_name)
+    return render_template("index.html", title = site_name, site_menu=site_menu)
 
-@app.route("/about")
+@app.route("/index")
+def kek():
+    return render_template("index.html", title = site_name, site_menu=site_menu)
+
+@app.route("/links")
 def about():
-    return "<h1>SASI!!!</h1>"
+    return render_template("links.html", title = "Сокращатель ссылок", site_menu=site_menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+#with app.test_request_context():
+# print(url_for("index"))
